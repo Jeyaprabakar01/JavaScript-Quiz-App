62f5967a1e52872b43d500243fe24ef86013493c
Jeyaprabakar01 <ej.prabakar@gmail.com>
2021-10-04 20:15:29 +0530

    Quiz App

---
+++ src/data.js
@@ -0,0 +1,333 @@
+export const data = [
+  {
+    id: 1,
+    question: "HTML stands for what?",
+    answers: [
+      {
+        text: "Hypertrophic Management Language",
+        correct: false,
+      },
+      {
+        text: "Hyperberic Tertiary Logrithm",
+        correct: false,
+      },
+      {
+        text: "Hypertext Markup Language",
+        correct: true,
+      },
+      {
+        text: "Hyperresonant Marginal Logrithm",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "HTML is what type of language ?",
+    answers: [
+      {
+        text: "Scripting Language",
+        correct: false,
+      },
+      {
+        text: "Markup Language",
+        correct: true,
+      },
+      {
+        text: "Programming Language",
+        correct: false,
+      },
+      {
+        text: "Network Protocol",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "HTML uses",
+    answers: [
+      {
+        text: "User defined tags",
+        correct: false,
+      },
+      {
+        text: "Pre-specified tags",
+        correct: false,
+      },
+      {
+        text: "Fixed tags defined by the language",
+        correct: true,
+      },
+      {
+        text: "Tags only for linking",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "The year in which HTML was first proposed _______",
+    answers: [
+      {
+        text: "1990",
+        correct: true,
+      },
+      {
+        text: "1980",
+        correct: false,
+      },
+      {
+        text: "2000",
+        correct: false,
+      },
+      {
+        text: "1995",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Fundamental HTML Block is known as ___________",
+    answers: [
+      {
+        text: "HTML Body",
+        correct: false,
+      },
+      {
+        text: "HTML Tag",
+        correct: true,
+      },
+      {
+        text: "HTML Attribute",
+        correct: false,
+      },
+      {
+        text: "HTML Element",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Apart from <b> tag, what other tag makes text bold ?",
+    answers: [
+      {
+        text: "<fat>",
+        correct: false,
+      },
+      {
+        text: "<strong>",
+        correct: true,
+      },
+      {
+        text: "<emp>",
+        correct: false,
+      },
+      {
+        text: "<black>",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "Who is Known as the father of World Wide Web (WWW)?",
+    answers: [
+      {
+        text: "Robert Cailliau",
+        correct: false,
+      },
+      {
+        text: "Tim Thompson",
+        correct: false,
+      },
+      {
+        text: "Charles Darwin",
+        correct: false,
+      },
+      {
+        text: "Tim Berners-Lee",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "What should be the first tag in any HTML document?",
+    answers: [
+      {
+        text: "<<head>>",
+        correct: false,
+      },
+      {
+        text: "<document>",
+        correct: false,
+      },
+      {
+        text: "<title>",
+        correct: false,
+      },
+      {
+        text: "<html>",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "How can you make a bulleted list with numbers?",
+    answers: [
+      {
+        text: "<dl>",
+        correct: false,
+      },
+      {
+        text: "<ol>",
+        correct: true,
+      },
+      {
+        text: "<list>",
+        correct: false,
+      },
+      {
+        text: "<ul>",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "What tag is used to display a picture in a HTML page?",
+    answers: [
+      {
+        text: "<picture>",
+        correct: false,
+      },
+      {
+        text: "<image>",
+        correct: false,
+      },
+      {
+        text: "<img>",
+        correct: true,
+      },
+      {
+        text: "<src>",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "HTML web pages can be read and rendered by _________.",
+    answers: [
+      {
+        text: "Compiler",
+        correct: false,
+      },
+      {
+        text: "Server",
+        correct: false,
+      },
+      {
+        text: "Web Browser",
+        correct: true,
+      },
+      {
+        text: "Interpreter",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Which HTML tag produces the biggest heading?",
+    answers: [
+      {
+        text: "<h9>",
+        correct: false,
+      },
+      {
+        text: "<h1>",
+        correct: true,
+      },
+      {
+        text: "<h6>",
+        correct: false,
+      },
+      {
+        text: "<h4>",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "HTML tags are surrounded by which type of brackets?",
+    answers: [
+      {
+        text: "Curly",
+        correct: false,
+      },
+      {
+        text: "Round",
+        correct: false,
+      },
+      {
+        text: "Angle",
+        correct: true,
+      },
+      {
+        text: "Squart",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "Tags and texts that are not directly displayed on the page are written in _____ section.",
+    answers: [
+      {
+        text: "<head>",
+        correct: true,
+      },
+      {
+        text: "<html>",
+        correct: false,
+      },
+      {
+        text: "<title>",
+        correct: false,
+      },
+      {
+        text: "<body>",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "Which of the following is not a browser ?",
+    answers: [
+      {
+        text: "Netscape Navigator",
+        correct: false,
+      },
+      {
+        text: "Microsofts Bing",
+        correct: true,
+      },
+      {
+        text: "Mozilla Firefox",
+        correct: false,
+      },
+      {
+        text: "Opera",
+        correct: false,
+      },
+    ],
+  },
+];
